255a366e5c3c6f144cc0698fd8d8ecf67a1b0ded
Dev Ted <chris.james88@icloud.com>
2024-09-01 00:08:43 -0500

    update using OpenWeatherMap and using python 3

--- weather.py
+++ weather.py
@@ -1,40 +1,43 @@
 ## get the weather condition (rainy, cloudy) for any given city
 ## by @limitedmage julianapena.com
 
-import urllib2
+import urllib.request
+import urllib.parse
+import json
 
-def getWeather(city):
-
-    #create google weather api url
-    url = "http://www.google.com/ig/api?weather=" + urllib2.quote(city)
+def get_weather(city, api_key):
+    # Create the URL for the OpenWeatherMap API
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={urllib.parse.quote(city)}&appid={api_key}&units=metric"
 
     try:
-        # open google weather api url
-        f = urllib2.urlopen(url)
-    except:
-        # if there was an error opening the url, return
-        return "Error opening url"
+        # Send a GET request to the API
+        with urllib.request.urlopen(url) as response:
+            # Read and decode the response
+            data = response.read().decode('utf-8')
+    except urllib.error.URLError as e:
+        # If there was an error, return the error message
+        return f"Error: {e.reason}"
 
-    # read contents to a string
-    s = f.read()
+    try:
+        # Parse JSON response
+        data_json = json.loads(data)
 
-    # extract weather condition data from xml string
-    weather = s.split("<current_conditions><condition data=\"")[-1].split("\"")[0]
+        # Check if the response contains the weather information
+        if data_json.get('cod') != 200:
+            return "Invalid city or error retrieving data"
 
-    # if there was an error getting the condition, the city is invalid
-    if weather == "<?xml version=":
-        return "Invalid city"
-
-    #return the weather condition
-    return weather
-
+        # Extract weather condition
+        weather = data_json['weather'][0]['description']
+        return f"Weather in {city}: {weather.capitalize()}"
+    except (json.JSONDecodeError, KeyError) as e:
+        return "Error parsing the weather data"
 
 def main():
-    #while True:
-       # city = raw_input("Give me a city: ")
-        print "Weather report in chennai"
-        weather = getWeather('madras')
-        print(weather)
+    # Your API key for OpenWeatherMap (sign up for a free key at https://openweathermap.org/)
+    api_key = 'YOUR_API_KEY_HERE'
+    
+    city = input("Give me a city: ")
+    print(get_weather(city, api_key))
 
 if __name__ == "__main__":
-    main()
+    main() 
